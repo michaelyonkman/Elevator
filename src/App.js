967174183440b59a9f318b8dcaa66ad2f84dc12c
Michael Yonkman <michaelyonkman@gmail.com>
2020-07-01 09:39:31 -0500

    added comments to code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 
+//keeping track of intervalID for setInterval function globally
 let intervalID;
 
 class App extends React.Component {
@@ -132,42 +133,54 @@ class App extends React.Component {
     );
   }
   handleClick(e) {
+    //disables clicked button
     this.setState({
       ['btn' + e.target.value + 'disabled']: true,
     });
+    //adds floor number to selected floors array
     this.state.selected.push(Number(e.target.value));
+    //sort selected floors array
     this.state.selected.sort((a, b) => {
       return a - b;
     });
     if (!this.state.direction) {
+      //logic to determine which direction the elevator will be going (up)
       if (Number(e.target.value) > this.state.floor) {
         this.setState({ direction: 'up', message: '▲' });
+        //looping through and disabling buttons for all floors less than current floor to prevent users from adding invalid inputs
         for (let i = 1; i < this.state.floor; i++) {
           this.setState({
             ['btn' + String(i) + 'disabled']: true,
           });
         }
+        //setInterval for moving up 1 floor per second
         intervalID = setInterval(() => {
           this.setState({
+            //disabling buttons as elevator ascends to keep users from adding invalid inputs
             floor: this.state.floor + 1,
             ['btn' + String(this.state.floor + 1) + 'disabled']: true,
           });
+          //if floor is in first position in selected floors array it's the first destination going up and calling arrived function
           if (this.state.floor === this.state.selected[0]) {
             this.arrived();
           }
         }, 1000);
+        //logic to determine elevator is going down
       } else if (Number(e.target.value) < this.state.floor) {
         this.setState({ direction: 'down', message: '▼' });
+        //looping through and disabling buttons for all floors greater than current floor to prevent users from adding invalid inputs
         for (let i = 10; i > this.state.floor; i--) {
           this.setState({
             ['btn' + String(i) + 'disabled']: true,
           });
         }
+        //setInterval to move down 1 floor per second
         intervalID = setInterval(() => {
           this.setState({
             floor: this.state.floor - 1,
             ['btn' + String(this.state.floor - 1) + 'disabled']: true,
           });
+          //if floor is in last position in selected floors array it's the first destination going down and calling arrived function
           if (
             this.state.floor ===
             this.state.selected[this.state.selected.length - 1]
@@ -179,46 +192,62 @@ class App extends React.Component {
     }
   }
   arrived() {
+    //play elevator bell audio clip on arrival
     this.playAudio();
+    //clearInterval to stop timer
     clearInterval(intervalID);
     this.setState({ message: '◀︎▶' });
+    //setTimeout for 5 sec pause at destination
     setTimeout(() => {
+      //checking if selected floors array has more destinations to go
       if (this.state.selected.length > 1) {
+        //if so, determine direction again (up)
         if (this.state.direction === 'up') {
           this.setState({ message: '▲' });
           this.state.selected.shift();
+          //setting 1 sec interval again
           intervalID = setInterval(() => {
             this.setState({
+              //disabling buttons again
               floor: this.state.floor + 1,
               ['btn' + String(this.state.floor + 1) + 'disabled']: true,
             });
+            //calling arrived if at destination
             if (this.state.floor === this.state.selected[0]) {
+              //calling arrived recursively until all destinations have been reached
               this.arrived();
             }
           }, 1000);
+          //determines direction is down
         } else if (this.state.direction === 'down') {
           this.setState({ message: '▼' });
           this.state.selected.pop();
+          //setting 1 sec interval again
           intervalID = setInterval(() => {
             this.setState({
+              //disabling buttons again
               floor: this.state.floor - 1,
               ['btn' + String(this.state.floor - 1) + 'disabled']: true,
             });
             if (
+              //calling arrived at destination
               this.state.floor ===
               this.state.selected[this.state.selected.length - 1]
             ) {
+              //calling arrived recursively until all destinations have been reached
               this.arrived();
             }
           }, 1000);
         }
       } else {
         this.state.selected.pop();
+        //if selected floors array is empty, enablind all buttons except current floor for next ride
         this.reset();
       }
     }, 5000);
   }
   reset() {
+    //enabling buttons between rides
     for (let i = 1; i <= 10; i++) {
       if (i === this.state.floor) {
         continue;
@@ -227,9 +256,11 @@ class App extends React.Component {
         ['btn' + String(i) + 'disabled']: false,
       });
     }
+    //setting direction back to null when ride's over
     this.setState({ direction: null });
   }
   playAudio() {
+    //function to play audio
     const ding = document.getElementById('ding');
     ding.play();
   }
